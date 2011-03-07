19d1f9e9f3307e90340d3224aefba4174597b0bd
André Millet <andremillet@gmail.com>
2011-03-07 16:59:16 -0300

    versao 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gabarito::Application.config.secret_token = '7158a7f40f79b80d7ee9eb2e08705dbc70b1af89cc4d012fbc9eef17b99128b2f9e4367af706b3641aed1c5d5445567d69e7f8bbac9954aeb99e92f3ea78e0ac'
